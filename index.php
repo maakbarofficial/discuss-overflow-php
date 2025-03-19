6070eabbc214a58451a8ed09494593af917d6eac
maakbarofficial <maakbarofficial@gmail.com>
2025-03-19 14:42:11 +0500

    Add project folder structure and files and Add header

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
-echo "DiscussOverflow"
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Discuss OverFlow</title>
+    <?php include('./client/common.php') ?>
+</head>
+<body>
+    <?php include('./client/header.php') ?>
+    
+</body>
+</html>
