7eb7328fe6fcec425b26b33011952c3a8003b966
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2025-09-06 22:39:17 +0500

    Update Debugger

--- debug.php
+++ debug.php
@@ -1,7 +1,12 @@
 <?php
 function debug($data, $die = false)
 {
+  $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
+  $file = $bt[0]['file'] ?? 'unknown file';
+  $line = $bt[0]['line'] ?? '?';
+
   echo '<pre style="background:#ffa500; color:#000; padding:10px; border-radius:5px;">';
+  echo "<strong>Debugger called at:</strong> $file : line $line\n\n";
   print_r($data);
   echo '</pre>';
 
