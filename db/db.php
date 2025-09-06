b843ae42c19ef385c40858c44450c59f3b4006bd
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2025-09-06 22:40:05 +0500

    Add database connection

---
+++ db/db.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../debug.php';
+
+$host = "localhost";
+$root = "root";
+$password = null;
+$database = "doapp";
+
+$conn = new mysqli($host, $root, $password, $database);
+
+if($conn->connect_error){
+  die("Database connection error" . $conn->connect_error);
+}
+
+echo "Databse connected";
+
+debug($conn);
+
+?>
