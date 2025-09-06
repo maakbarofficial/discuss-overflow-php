ee4ed9b4e6056b03470f9852984007995a30b4dc
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2025-09-06 22:22:35 +0500

    Add post request in signup.php

--- client/signup.php
+++ client/signup.php
@@ -5,25 +5,25 @@
                 <h3>Sign Up</h3>
             </div>
             <div>
-                <form>
+                <form method="POST" action="<?php echo $baseUrl ?>server/requests.php">
                     <div>
                         <label for="username">Username</label>
-                        <input type="text" id="username" placeholder="Enter your username" required>
+                        <input type="text" name="username" placeholder="Enter your username" required>
                     </div>
 
                     <div>
                         <label for="email">Email Address</label>
-                        <input type="email" id="email" placeholder="Enter your email" required>
+                        <input type="email" name="email" placeholder="Enter your email" required>
                     </div>
 
                     <div>
                         <label for="password">Password</label>
-                        <input type="password" id="password" placeholder="Enter your password" required>
+                        <input type="password" name="password" placeholder="Enter your password" required>
                     </div>
 
                     <div>
                         <label for="role">Role</label>
-                        <select id="role" required>
+                        <select name="role" required>
                             <option selected>Select role</option>
                             <option value="admin">Admin</option>
                             <option value="user">User</option>
@@ -32,7 +32,7 @@
                     </div>
 
                     <div>
-                        <button type="submit">Sign Up</button>
+                        <button type="submit" name="signup">Sign Up</button>
                     </div>
                 </form>
             </div>
